202f7c0b082148465a45d514045f3ca5cd42e565
MKanunnikau <matea.ekv@gmail.com>
2026-05-21 22:59:40 +0200

    add styling to dogs

--- app/dogs/page.js
+++ app/dogs/page.js
@@ -1,23 +1,20 @@
-"use client";
-import Image from "next/image";
-import { useState } from "react";
+export default async function Dog() {
+  const response = await fetch("https://dog.ceo/api/breeds/image/random");
+  const data = await response.json();
 
-export default function Dog() {
-  const [image, setImage] = useState(null);
-  async function fetchRandomDog() {
-    const response = await fetch("https://dog.ceo/api/breeds/image/random");
-    if (!response.ok) {
-      throw new Error({ response });
-    }
-    const data = await response.json();
-    setImage(data.message);
-  }
   return (
-    <>
-      <button onClick={fetchRandomDog}>Get a dog</button>
-      {image && (
-        <img src={image} alt="random dog image" width="500" height="500" />
-      )}
-    </>
+    <div className="thePage">
+      <h1>
+        Here is a random image of a dog. Refresh the page for another one!
+      </h1>
+
+      <img
+        className="image"
+        src={data.message}
+        alt="random dog image"
+        width="500"
+        height="500"
+      />
+    </div>
   );
 }
